abdfb908dbaa0d37aa8ed387a4ff03ed50334dda
Young Il PARK <73331241+ChicagoPark@users.noreply.github.com>
2022-05-31 17:59:50 +0900

    Update 1_Lane_2D.py

--- 1_Lane_2D.py
+++ 1_Lane_2D.py
@@ -7,15 +7,11 @@ model_path = "../models/tusimple_18.pth"
 model_type = ModelType.TUSIMPLE
 use_gpu = False
 
-#image_path = "input.jpg"
 sn = int(sys.argv[1]) if len(sys.argv)>1 else 7 #default 0-7517
 name = '%06d'%sn # 6 digit zeropadding
 image_path = f'../../../../dataset/training/image_2/{name}.png'
 csv_path = "../../CSV_Communication/1_lane.csv"
 
-#print(model_type)
-#print(model_type.griding_num)
-#print(model_type.cls_num_per_lane)
 
 # Initialize lane detection model
 lane_detector = UltrafastLaneDetector(model_path, model_type, use_gpu)
